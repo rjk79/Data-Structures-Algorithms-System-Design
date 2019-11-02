d5ca61f7d1b46dad72cbcb9aae89f4b0809abf44
“robertku” <robertku79@gmail.com>
2019-11-02 11:55:29 -0400

    Add notes

--- 59_spiral_matrix.py
+++ 59_spiral_matrix.py
@@ -1,6 +1,7 @@
 def generateMatrix(n):
     A, lo = [], n*n+1
     while lo > 1:
+        import pdb; pdb.set_trace()
         lo, hi = lo - len(A), lo
         A = [range(lo, hi)] + zip(*A[::-1])
     return A
