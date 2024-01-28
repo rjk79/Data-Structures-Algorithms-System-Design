41d63e945fb690ef66cd72f3d83d172febd9a3c8
rjk79 <robertku79@gmail.com>
2024-01-28 13:06:05 -0500

    Remove problems

--- other/wines.py
+++
@@ -1,8 +0,0 @@
-wines = [1, 4, 2, 3]
-N = 4
-def recurse(wines):
-    if not wines: return 0
-    print(wines)
-    year = N - len(wines) + 1
-    return max(year * wines[0] + recurse(wines[1:]), year * wines[-1] + recurse(wines[:-1]))
-print(recurse(wines))
